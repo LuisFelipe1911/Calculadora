702f562bab9900d9da1e119ff1024b4455a7f52f
LuisFelipe1911 <140674434+LuisFelipe1911@users.noreply.github.com>
2024-03-14 18:20:20 -0300

    Adicionado Explicação de Código

--- main.js
+++ main.js
@@ -21,57 +21,95 @@
 // SOFTWARE.
 
 
-
+// exibe um alerta de boas vindas
 alert("Seja Bem Vindo A Calculadora!");
 
+// armazena o simbolo da operação que o usuario deseja utilizar
 let operation = prompt("Diga Qual Operador Deseja Usar, diga em simbolos: +-*/");
+
+// armazena um local na memória chamado n1 e armazena dentro dela o valor 0
 let n1 = 0;
+// armazena um local na memória chamado n2 e armazena dentro dela o valor 0
 let n2 = 0;
+// armazena um local na memória chamado result e armazena dentro dela o valor 0
 let result = 0;
 
-
+// se operation for igual a +
 if (operation == "+") {
+    // n1  recebe método de receber dados digitados pelo usuario e converte para um numero Inteiro
     n1 = parseInt(prompt("Diga O Primeiro Numero:"));
+
+    // n2  recebe método de receber dados digitados pelo usuario e converte para um numero Inteiro
     n2 = parseInt(prompt("Diga O Segundo Numero:"));
-  
+
+
+    // faz a soma do valor da váriavel n1 e soma com a n2
     result = n1 + n2;
 
+
+    // exibe o resultado
     alert("Seu Resultado é: " + result + ".");
 
     
 
 }
+
+// e se operation for igual a -
 else if (operation == "-") {
+
+    // n1  recebe método de receber dados digitados pelo usuario e converte para um numero Inteiro
     n1 = parseInt(prompt("Diga O Primeiro Numero:"));
+
+    // n2  recebe método de receber dados digitados pelo usuario e converte para um numero Inteiro
     n2 = parseInt(prompt("Diga O Segundo Numero:"));
-  
+
+    // subtrai o valor de n1 e n2 e armazena na variavel result
     result = n1 - n2;
 
+    
     alert("Seu Resultado é: " + result + ".");
 
     
 }
+
+// e se operation for igual *
 else if(operation == "*") {
+
+    // n1 recebe método de receber dados do usuario e converte para um numero Inteiro
     n1 = parseInt(prompt("Diga O Primeiro Numero:"));
+
+    // n2 recebe método de receber dados do usuario e converte para um numero Inteiro
     n2 = parseInt(prompt("Diga O Segundo Numero:"));
-  
+
+    // multiplica n1 e n2 e armazena o resultado na variavel result
     result = n1 * n2;
 
+
+    
     alert("Seu Resultado é: " + result + ".");
 
     
 }
 
+
+// e se operation for igual a /
 else if (operation == "/") {
+    // n1 recebe método de receber dados do usuario e converte para um numero Inteiro
     n1 = parseInt(prompt("Diga O Primeiro Numero:"));
+
+    // n2 recebe método de receber dados do usuario e converte para um numero Inteiro
     n2 = parseInt(prompt("Diga O Segundo Numero:"));
-  
+
+    // divide o n1 com n2 e armazena o valor
     result = n1 / n2;
 
     alert("Seu Resultado é: " + result + ".");
 
    
 }
+
+
+// ele cairá nesse else se os dados recebidos não forem igual aos anteriores
 else {
   alert("Erro De Comando. Tente Novamente.");
 
